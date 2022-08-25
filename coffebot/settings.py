50b8ef778cbae7223761ae143b1dceefb6d6a4cf
Ubuntu <ubuntu@ip-172-26-7-245.eu-west-2.compute.internal>
2022-08-25 14:03:59 +0000

    1

--- coffebot/settings.py
+++ coffebot/settings.py
@@ -16,7 +16,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+CSRF_TRUSTED_ORIGINS = [
+    'http://13.40.242.143:1337'
+]
 
 
 # Quick-start development settings - unsuitable for production
